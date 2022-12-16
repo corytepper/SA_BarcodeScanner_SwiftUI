1ac3e37f7cebf18e0d239837d472c9a27f95d2af
Cory Tepper <corytepper@gmail.com>
2022-12-16 14:21:33 -0500

    Initial Commit

---
+++ SA_BarcodeScanner_SwiftUI/SA_BarcodeScanner_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SA_BarcodeScanner_SwiftUIApp.swift
+//  SA_BarcodeScanner_SwiftUI
+//
+//  Created by Cory Tepper on 12/16/22.
+//
+
+import SwiftUI
+
+@main
+struct SA_BarcodeScanner_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            BarcodeScannerView()
+        }
+    }
+}
